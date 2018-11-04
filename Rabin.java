33dcf967a1ca0669da135647a9140cbc851979d7
GH05Tx <43876190+GH05Tx@users.noreply.github.com>
2018-11-04 23:18:45 +0100

    Add files via upload

--- Rabin.java
+++ Rabin.java
@@ -10,9 +10,15 @@ import java.nio.file.Paths;
 import java.util.Random;
 
 
+///maina jeszcze nie ruszyłem - uzupełnie go adekwatnie do funkcji stąd
+///dodam jeszcze jedno okno do wyświetlania klucza publicznego
+///nie rozkminiłem nadal czy konwertowanie wszystkiego z tab byte na jakieś
+///łańcuchy nie byłoby dobrym posunięciem
+///chyba że ogarniemy jak wybierać który z 4 pierwiastków jest poprawny 
+
 public class Rabin {
 
-    ///losowanie klucza prywatnego P=3(mod 4)
+    ///losowanie klucza prywatnego P,Q=3(mod 4)
     public byte getP(){
         Random rand = new Random();
         int i=rand.nextInt(29)*4+3;
@@ -37,23 +43,25 @@ public class Rabin {
         }
         return ciphered;
     }
-    ///Algorytm euklidesa odnajdywania NWD 
+    ///Algorytm euklidesa odnajdywania NWD
+    ///wykorzystywany przy odszyfrowywaniu
     public int gcd(int a, int b) {
         if (a == 0)
             return b;
-    
+
         while (b != 0) {
             if (a > b)
                 a = a - b;
             else
                 b = b - a;
         }
-        int tab[]=new int[2]; 
+        int tab[]=new int[2];
         tab[0]=a;
         tab[1]=b;
         return tab;
     }
-
+///ta kurwa jeszcze wymaga troche pracy
+///te inty na koniec przekonwertuje do byte - przy obliczeniach na bajtach wypierdalało mi błąd
     public byte[] decipher(byte[] ciphered, byte n, byte p, byte q){
 
         byte[] deciphered = new byte[ciphered.length];
@@ -65,7 +73,7 @@ public class Rabin {
         int yq[]=new int[ciphered.length];
         int tmp[]=new int[2];
         ///4 pierwiastki kwadratowe z c(mod n)
-        ///tylko jeden z nich zawiera poprawną wiadomość 
+        ///tylko jeden z nich zawiera zaszyfrowaną wiadomość
         int x1[]=new int[ciphered.length];
         int x2[]=new int[ciphered.length];
         int x3[]=new int[ciphered.length];
@@ -73,11 +81,13 @@ public class Rabin {
 
         for(int i=0 ;i<ciphered.length ;i++)
         {
+///zabawa zaczyna się od obliczenia pierwiastków kwadratowych liczby c(mod p) i c(mod q)
+///tzn mp=c^((p+1)/4)(mod p) i mq=c^((q+1)/4)(mod q)
             mp1[i]=(Math.pow(ciphered[i], (p+1)/4))%p;
             mp2[i]=p-mp1[i];
             mq1[i]=(Math.pow(ciphered[i], (q+1)/4))%q;
             mq2[i]=q-mq1[i];
-
+///wykorzystujemy algo euklidesa żeby wyznaczyć liczby spełniające warunek yp*p+yq*q=1
             tmp=gcd(p,q);
             yp[i]=tmp[0];
             yq[i]=tmp[1];
