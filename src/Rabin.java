a763234b62699a66388d5a8182a665e15e93b690
lukko98@gmail.com <lukko98@gmail,com>
2018-11-23 20:18:24 +0100

    sory mordeczko za wylew tera juz aktualne powinno byc

--- src/Rabin.java
+++ src/Rabin.java
@@ -31,86 +31,17 @@ public class Rabin {
         BigInteger P;
         Random rnd = new Random();
         do {
-            P = BigInteger.probablePrime(256, rnd);
+            P = BigInteger.probablePrime(512, rnd);
+            //P.isProbablePrime(100);
             //System.out.println("Heja");
-        } while (!P.mod(FOUR).equals(THREE));
+        } while (!P.mod(FOUR).equals(THREE) && P.isProbablePrime(10000));
         return P;
 
     }
 
-    public boolean returnPrime(BigInteger number) {
-        //check via BigInteger.isProbablePrime(certainty)
-        if (!number.isProbablePrime(5))
-            return false;
-
-        //check if even
-        BigInteger two = new BigInteger("2");
-        if (!two.equals(number) && BigInteger.ZERO.equals(number.mod(two)))
-            return false;
-
-        //find divisor if any from 3 to 'number'
-        for (BigInteger i = new BigInteger("3"); i.multiply(i).compareTo(number) < 1; i = i.add(two)) {//start from 3, 5, etc. the odd number, and look for a divisor if any
-            System.out.println("I: " + i);
-            System.out.println("P: " + number);
-            if (BigInteger.ZERO.equals(number.mod(i))) //check if 'i' is divisor of 'number'
-                return false;
-        }
-        return true;
-    }
-
-    public boolean Miller_Rabin(BigInteger P){
-        Random rnd = new Random();
-        int size = P.bitLength();
-        BigInteger x = new BigInteger(size, rnd);
-        System.out.println("P to: " + P);
-        System.out.println("X to: " + x);
-        BigInteger stepone = P.gcd(x);
-        System.out.println("GCD x i P: " + stepone);
-        BigInteger one = new BigInteger("1");
-        BigInteger negativeone = new BigInteger("-1");
-        BigInteger two = new BigInteger("2");
-        BigInteger zero = new BigInteger("0");
-        BigInteger m = P;
-        m = P.subtract(one);
-        int i=0;
-        boolean z = true;
-        while(z)
-        {
-            if(m.mod(two).equals(zero)){
-                m = m.divide(two);
-                System.out.println("m mod 2 " + P);
-                i++;
-                System.out.println("i " + i);
-            }
-            else{
-                z = false;
-            }
-        }
-
-
-        System.out.println("P to: " + P);
-        System.out.println("m to: " + m);
-        System.out.println("i to: " + i);
-        System.out.println("P-1 = 2^" + i + "*" + m);
-
-        BigInteger y = x.modPow(m,P);
-        System.out.println("Y to: " + y);
-
-        //while(!y.equals(one) || !y.equals(negativeone)){
-         for( int klej =0 ; klej < 100; klej++) {
-
-
-             y = y.modPow(two, P);
-             System.out.println("Y jest równy: " + y);
-         }
-        //}
-        return true;
-    }
-
-
     ///wyznaczanie klucza publicznego N=P*Q
     public BigInteger getN(BigInteger p, BigInteger q){
-        BigInteger tmp1, tmp2, n;
+        BigInteger n;
         n=p.multiply(q);
         return n;
     }
@@ -157,9 +88,7 @@ public class Rabin {
         BigInteger mp2[]=new BigInteger[ciphered.length];
         BigInteger mq1[]=new BigInteger[ciphered.length];
         BigInteger mq2[]=new BigInteger[ciphered.length];
-        BigInteger yp[]=new BigInteger[ciphered.length];
-        BigInteger yq[]=new BigInteger[ciphered.length];
-        BigInteger pom, pom2, pom3;
+        BigInteger pom, pom2, pom3, yp, yq;
         BigInteger tmp[]=new BigInteger[2];
         ///4 pierwiastki kwadratowe z c(mod n)
         ///tylko jeden z nich zawiera zaszyfrowaną wiadomość
@@ -168,12 +97,22 @@ public class Rabin {
         BigInteger x3[]=new BigInteger[ciphered.length];
         BigInteger x4[]=new BigInteger[ciphered.length];
 
+        tab[0] = BigInteger.valueOf(0);
+        tab[1] = BigInteger.valueOf(1);
+        BigInteger FOUR = new BigInteger("4");
+        pom = p.add(BigInteger.ONE);
+        pom = pom.divide(FOUR);
+        pom2 = q.add(BigInteger.ONE);
+        pom2 = pom2.divide(FOUR);
+        tmp=gcd(p,q);
+        yp=tmp[1];
+        yq=tmp[0];
         for(int i=0 ;i<ciphered.length ;i++)
         {
 ///zabawa zaczyna się od obliczenia pierwiastków kwadratowych liczby c(mod p) i c(mod q)
 ///tzn mp=c^((p+1)/4)(mod p) i mq=c^((q+1)/4)(mod q)
 
-            BigInteger FOUR = new BigInteger("4");
+            //BigInteger FOUR = new BigInteger("4");
             pom = p.add(BigInteger.ONE);
             pom = pom.divide(FOUR);
             mp1[i] = ciphered[i].modPow(pom, p);
@@ -182,17 +121,14 @@ public class Rabin {
             pom2 = pom2.divide(FOUR);
             mq1[i] = ciphered[i].modPow(pom2, q);
 ///wykorzystujemy algo euklidesa żeby wyznaczyć liczby spełniające warunek yp*p+yq*q=1
-            tab[0] = BigInteger.valueOf(0);
-            tab[1] = BigInteger.valueOf(1);
 
-            tmp=gcd(p,q);
-            yp[i]=tmp[1];
-            yq[i]=tmp[0];
-            //System.out.println("Yp "+ yp[i]);
-            //System.out.println("Yq "+ yq[i]);
 
-            pom = (yp[i].multiply(p)).multiply(mq1[i]);
-            pom2 = (yq[i].multiply(q)).multiply(mp1[i]);
+
+            //System.out.println("Yp "+ yp);
+            //System.out.println("Yq "+ yq);
+
+            pom = (yp.multiply(p)).multiply(mq1[i]);
+            pom2 = (yq.multiply(q)).multiply(mp1[i]);
             pom3 = pom.add(pom2);
             x1[i] = pom3.mod(n);
             x2[i] = n.subtract(x1[i]);
